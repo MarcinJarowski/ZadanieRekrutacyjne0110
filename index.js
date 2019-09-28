59cf8b7de8dd81290eabb422b183351eb385e7f6
Marcin Jarowski <jarowski.marcin@yahoo.co.uk>
2019-09-28 17:06:06 +0200

    basic masonry grid

--- index.js
+++ index.js
@@ -9,22 +9,20 @@ const requestData = (url, functionHandler) => {
     .then(functionHandler)
     .catch(error => console.log(error.message));
 };
-
-const galleryWrapper = document.querySelector("#gallery");
 const galleryList = document.querySelector(".gallery__list");
 
 const randomDate = () => {
   const getRandomArbitrary = (min, max) => Math.random() * (max - min) + min;
-  const date1 = new Date("01/01/2000").getTime();
+  const date1 = new Date("01/01/2010").getTime();
   const date2 = new Date("01/10/2019").getTime();
   return new Date(getRandomArbitrary(date1, date2)).toLocaleDateString();
 };
 
 const createList = data => {
-  console.log(data);
-  let pictureObjectsArray = data.hits;
+  const pictureObjectsArray = data.hits;
   pictureObjectsArray
     .map(element => {
+      console.log(element);
       const galleryListItem = document.createElement("li");
       const galleryImage = document.createElement("img");
       const galleryImageTitle = document.createElement("h3");
@@ -33,17 +31,21 @@ const createList = data => {
       galleryListItem.appendChild(galleryImage);
       galleryListItem.appendChild(galleryImageTitle);
       galleryListItem.appendChild(galleryImageSubtitle);
-
+      galleryListItem.setAttribute("class", "gallery__listitem");
       galleryImage.setAttribute("src", element.largeImageURL);
-      galleryImageTitle.innerText = element.user;
-      galleryImageSubtitle.innerText = randomDate();
+      galleryImage.setAttribute("class", "gallery__image");
+      galleryImageTitle.setAttribute("class", "gallery__image__title");
+      galleryImageSubtitle.setAttribute("class", "gallery__image__subtitle");
+
+      galleryImageSubtitle.innerText = `Dodane przez: ${element.user}`;
+      galleryImageTitle.innerText = randomDate();
 
       return galleryListItem;
     })
-    .forEach(element => galleryList.appendChild(element));
+    .forEach(galleryListItem => galleryList.appendChild(galleryListItem));
 };
 
 requestData(
-  "https://pixabay.com/api/?key=13774470-6c5a921daa2ecf77f068a8231&per_page=21&page=3",
+  "https://pixabay.com/api/?key=13774470-6c5a921daa2ecf77f068a8231&per_page=20&page=20",
   createList
 );
