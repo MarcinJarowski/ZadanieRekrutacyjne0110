a1832cacc9694b9dd06b0596ecde456515e55f59
Marcin Jarowski <jarowski.marcin@yahoo.co.uk>
2019-10-01 11:08:27 +0200

    css to sass

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
+const galleryList = document.querySelector(".gallery__list");
+const gallerySection = document.querySelector("#gallery");
+const createModal = document.createElement("div");
+const createImg = document.createElement("img");
+const closeModalButton = document.createElement("button");
+
 const requestData = (url, functionHandler) => {
   fetch(url)
     .then(response => {
       if (response.ok) {
+        console.log(response.status);
         return response.json();
       }
       throw new Error("Response in not OK");
@@ -9,12 +16,10 @@ const requestData = (url, functionHandler) => {
     .then(functionHandler)
     .catch(error => console.log(error.message));
 };
-const galleryList = document.querySelector(".gallery__list");
-const gallerySection = document.querySelector("#gallery");
-const modal = document.createElement("div");
-const createImg = document.createElement("img");
-const closeModalButton = document.createElement("button");
-
+const preloadImages = url => {
+  let img = new Image();
+  img.src = url;
+};
 const randomDate = () => {
   const getRandomArbitrary = (min, max) => Math.random() * (max - min) + min;
   const date1 = new Date("01/01/2010").getTime();
@@ -22,24 +27,28 @@ const randomDate = () => {
   return new Date(getRandomArbitrary(date1, date2)).toLocaleDateString();
 };
 
-const createModal = url => {
-  gallery.appendChild(modal);
-  modal.setAttribute("class", "gallery__modal--open");
-  modal.appendChild(createImg);
+const appendModal = url => {
+  gallery.appendChild(createModal);
+  createModal.setAttribute("class", "gallery__modal--open");
+  createModal.appendChild(createImg);
   createImg.setAttribute("class", "gallery__modal__img");
   createImg.setAttribute("src", url);
   createModalCloseButton();
 };
+const deleteModal = () => {
+  // let closeButton = document.querySelector(".gallery__modal__closeButton");
+  //remove event listener
+  gallerySection.removeChild(document.querySelector(".gallery__modal--open"));
+};
 const createModalCloseButton = () => {
   closeModalButton.setAttribute("class", "gallery__modal__closeButton");
-  modal.appendChild(closeModalButton);
-  closeModalButton.innerText = "ZAMKNIJ";
-  closeModalButton.addEventListener("click", () => {
-    gallerySection.removeChild(document.querySelector(".gallery__modal--open"));
-  });
+  createModal.appendChild(closeModalButton);
+  closeModalButton.innerText = "X";
+  closeModalButton.addEventListener("click", () => deleteModal());
 };
 const createList = data => {
   const pictureObjectsArray = data.hits;
+
   pictureObjectsArray
     .map(element => {
       const galleryListItem = document.createElement("li");
@@ -47,29 +56,32 @@ const createList = data => {
       const galleryImageTitle = document.createElement("h3");
       const galleryImageSubtitle = document.createElement("h5");
 
-      galleryListItem.appendChild(galleryImage);
-      galleryListItem.appendChild(galleryImageTitle);
-      galleryListItem.appendChild(galleryImageSubtitle);
-
-      galleryListItem.setAttribute("class", "gallery__listitem");
       galleryImage.setAttribute("src", element.largeImageURL);
       galleryImage.setAttribute("class", "gallery__image");
+      galleryImage.addEventListener("load", e => {
+        galleryListItem.appendChild(galleryImage);
+        galleryListItem.appendChild(galleryImageTitle);
+        galleryListItem.appendChild(galleryImageSubtitle);
+        galleryImage.addEventListener("click", () => {
+          appendModal(element.largeImageURL);
+        });
+      });
+
+      galleryListItem.setAttribute("class", "gallery__listitem");
       galleryImageTitle.setAttribute("class", "gallery__image__title");
       galleryImageSubtitle.setAttribute("class", "gallery__image__subtitle");
-
-      galleryImage.addEventListener("click", event => {
-        createModal(element.largeImageURL);
-      });
 
       galleryImageSubtitle.innerText = `Dodane przez: ${element.user}`;
       galleryImageTitle.innerText = randomDate();
 
       return galleryListItem;
     })
-    .forEach(galleryListItem => galleryList.appendChild(galleryListItem));
+    .forEach(galleryListItem => {
+      galleryList.appendChild(galleryListItem);
+    });
 };
 
 requestData(
-  "https://pixabay.com/api/?key=13774470-6c5a921daa2ecf77f068a8231&per_page=20&page=6",
+  "https://pixabay.com/api/?key=13774470-6c5a921daa2ecf77f068a8231&per_page=20&page=13",
   createList
 );
